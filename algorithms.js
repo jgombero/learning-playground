6a608fab138221a130a4c7d97a7b83a6d5b9eb43
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-27 16:24:34 -0700

    Add function to demonstrate binary search

--- algorithms.js
+++ algorithms.js
@@ -44,3 +44,29 @@ const linearSearch = (list, item) => {
 console.log(linearSearch([2, 5, 7, 90, 103], 90)); // 3
 console.log(linearSearch([2, 5, 7, 90, 103], 91)); // -1
 
+// Binary Search (divide and conquer)
+const binarySearch = (list, item) => {
+  let min = 0;
+  let max = list.length - 1;
+  let guess;
+
+  while (min <= max) {
+    guess = Math.floor((min + max) / 2);
+
+    if (list[guess] === item) {
+      return guess;
+    } else {
+      if (list[guess] < item) {
+        min = guess + 1;
+      } else {
+        max = guess - 1;
+      }
+    }
+  }
+
+  return -1;
+}
+
+console.log(binarySearch([2, 6, 7, 90, 103], 90)); // 3
+console.log(binarySearch([2, 6, 7, 90, 103], 91)); // -1
+
