ff32447f2b36e3d0344b28f3e8fcca136f60a1b6
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-27 16:21:08 -0700

    Add roughly what happens under the hood with classes

--- new.js
+++ new.js
@@ -34,3 +34,18 @@ const user2 = new UserCreator("Batman", 99);
 // console.log(UserCreator);
 
 
+// What happens under the hood when we use classes
+function userCreator(name, score) {
+  this.name = name;
+  this.score = score;
+};
+
+userCreator.prototype.increment = function() { this.score++ };
+userCreator.prototype.login = function() { console.log("login") };
+
+const user3 = new userCreator("Josh", 4);
+console.log(user3);
+user3.increment();
+console.log(user3);
+console.log(userCreator.prototype);
+console.log(user3.__proto__);
